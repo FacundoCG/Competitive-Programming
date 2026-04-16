c1110cc3d05cc5eb163d9d299af0e8842744ebf8
FacundoCG <coronelfacundo30@gmail.com>
2026-04-16 20:28:11 -0300

    Mixtures

---
+++ Homework/Mixtures.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using ii = pair<int,int>;
+
+template <typename T>
+using vv = vector<vector<T>>;
+
+const ll UNDEFINED = -1;
+const int MAX_N = 1e5 + 1;
+const int MOD = 100;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const ll zero = 0;
+const ld EPSILON = 1e-10;
+const double PI = acos(-1.0);
+
+#define pb push_back
+#define fst first
+#define snd second
+#define esta(x,c) ((c).find(x) != (c).end())  // Devuelve true si x es un elemento de c.
+#define all(c) (c).begin(),(c).end()
+#define SIZE(c) int((c).size())
+
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define RAYA cerr << "----------" << endl
+
+#define forn(i,n) for (int i=0;i<(int)(n);i++)
+#define forsn(i,s,n) for (int i=(s);i<(int)(n);i++)
+#define dforn(i,n) for(int i=(int)((n)-1);i>=0;i--)
+#define dforsn(i,s,n) for(int i=(int)((n)-1);i>=(int)(s);i--)
+
+// Show pair
+template <typename T1, typename T2>
+ostream & operator <<(ostream &os, const pair<T1, T2> &p) {
+    os << "{" << p.first << "," << p.second << "}";
+    return os;
+}
+
+// Show vector
+template <typename T>
+ostream & operator <<(ostream &os, const vector<T> &v) {
+    os << "[";
+    forn(i, v.size()) {
+        if (i > 0) os << ",";
+        os << v[i];
+    }
+    return os << "]";
+}
+
+// Show set
+template <typename T>
+ostream & operator <<(ostream &os, const set<T> &s) {
+    os << "{";
+    for(auto it = s.begin(); it != s.end(); it++){
+        if(it != s.begin()) os << ",";
+        os << *it;
+    }
+    return os << "}";
+}
+
+// ############################################################### //
+
+ll mod(ll a, ll m = MOD){
+	ll res = a % m;
+	if (res < 0) res += m;
+	return res;
+}
+
+ll addMod(ll a, ll b, ll m = MOD){
+	ll res = mod(a, m) + mod(b, m);
+	return mod(res, m);
+}
+
+ll query(int l, int r, vl &prefixSum){
+	assert(0 <= l && l <= r && r < SIZE(prefixSum));
+	if (l == 0) return prefixSum[r];
+	return addMod(prefixSum[r], -prefixSum[l-1]);
+}
+
+int n; 
+
+ll dp(int i, int j, vl &A, vl &prefixSum, vvl &memo){
+	if (i >= j) return 0;
+	
+	if (memo[i][j] == UNDEFINED){
+		memo[i][j] = LINF;
+		
+		forsn(l, i, j){ // [i, l] y [l+1, j]
+			ll a = query(i, l, prefixSum);
+			ll b = query(l+1, j, prefixSum);
+			memo[i][j] = min(memo[i][j], a*b + dp(i, l, A, prefixSum, memo) + dp(l+1, j, A, prefixSum, memo));
+		}
+	}
+	
+	return memo[i][j];
+}
+ 
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+	
+	while (cin >> n){
+		vl A(n); forn(i, n) cin >> A[i];
+		vvl memo(n, vl(n, UNDEFINED));
+				
+		vl prefixSum(n);
+		prefixSum[0] = A[0];
+		forsn(i, 1, n) prefixSum[i] = addMod(prefixSum[i-1], A[i]);
+		ll res = dp(0, n-1, A, prefixSum, memo);
+		cout << res << "\n";
+	}
+	
+	return 0;
+}
