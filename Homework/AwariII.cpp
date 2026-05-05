16cbdc10ab54e7179d5f71f648592493892fe173
FacundoCG <coronelfacundo30@gmail.com>
2026-05-05 20:40:15 -0300

    Awari II

---
+++ Homework/AwariII.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using ii = pair<ll,ll>;
+using vii = vector<ii>;
+using vvii = vector<vii>;
+
+template <typename T>
+using vv = vector<vector<T>>;
+
+const ll UNDEFINED = -1;
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e8;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const ll zero = 0;
+const ld EPSILON = 1e-10;
+const double PI = acos(-1.0);
+
+#define pb push_back
+#define fst first
+#define snd second
+#define esta(x,c) ((c).find(x) != (c).end())  // Devuelve true si x es un elemento de c.
+#define all(c) (c).begin(),(c).end()
+#define SIZE(c) int((c).size())
+
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define RAYA cerr << "----------" << endl
+
+#define forn(i,n) for (int i=0;i<(int)(n);i++)
+#define forsn(i,s,n) for (int i=(s);i<(int)(n);i++)
+#define dforn(i,n) for(int i=(int)((n)-1);i>=0;i--)
+#define dforsn(i,s,n) for(int i=(int)((n)-1);i>=(int)(s);i--)
+
+// Show pair
+template <typename T1, typename T2>
+ostream & operator <<(ostream &os, const pair<T1, T2> &p) {
+    os << "{" << p.first << "," << p.second << "}";
+    return os;
+}
+
+// Show vector
+template <typename T>
+ostream & operator <<(ostream &os, const vector<T> &v) {
+    os << "[";
+    forn(i, v.size()) {
+        if (i > 0) os << ",";
+        os << v[i];
+    }
+    return os << "]";
+}
+
+// Show set
+template <typename T>
+ostream & operator <<(ostream &os, const set<T> &s) {
+    os << "{";
+    for(auto it = s.begin(); it != s.end(); it++){
+        if(it != s.begin()) os << ",";
+        os << *it;
+    }
+    return os << "}";
+}
+
+// ############################################################### //
+
+int n; 
+
+void solve(){	
+	vi A(n+1); forsn(i, 1, n+1) cin >> A[i];
+	
+	stack<int> s;
+	
+	dforsn(i, 1, n+1) {
+		if (A[i] == i) s.push(i);
+		if (A[i] > i) { cout << "N\n"; return ; }
+	}
+	
+	while (!s.empty()){
+		int index = s.top(); s.pop();
+		A[index] = 0;
+		
+		dforsn(i, 1, index){
+			A[i]++;
+			if (A[i] == i) s.push(i);
+			assert(A[i] <= i);
+		}
+	}
+	
+	forsn(i, 1, n+1) if (A[i] != 0) { cout << "N\n"; return ;}
+	cout << "S\n";
+}
+
+int main()
+{
+	while (cin >> n){
+		if (n == -1) break;
+		solve();
+	}
+	return 0;
+}
