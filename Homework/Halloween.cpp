16ad4e18bcd76e555100908b7f1699e1c34559fd
FacundoCG <coronelfacundo30@gmail.com>
2026-05-06 14:39:31 -0300

    Halloween

---
+++ Homework/Halloween.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vvvl = vector<vvl>;
+using ii = pair<int,int>;
+using vii = vector<ii>;
+using vvii = vector<vii>;
+
+template <typename T>
+using vv = vector<vector<T>>;
+
+const ll UNDEFINED = -1;
+//~ const int MAX_N = 1e5 + 1;
+int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const ll zero = 0;
+const ld EPSILON = 1e-10;
+const double PI = acos(-1.0);
+
+#define pb push_back
+#define fst first
+#define snd second
+#define esta(x,c) ((c).find(x) != (c).end())  // Devuelve true si x es un elemento de c.
+#define all(c) (c).begin(),(c).end()
+#define SIZE(c) int((c).size())
+
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define RAYA cerr << "----------" << endl
+
+#define forn(i,n) for (int i=0;i<(int)(n);i++)
+#define forsn(i,s,n) for (int i=(s);i<(int)(n);i++)
+#define dforn(i,n) for(int i=(int)((n)-1);i>=0;i--)
+#define dforsn(i,s,n) for(int i=(int)((n)-1);i>=(int)(s);i--)
+
+// Show pair
+template <typename T1, typename T2>
+ostream & operator <<(ostream &os, const pair<T1, T2> &p) {
+    os << "{" << p.first << "," << p.second << "}";
+    return os;
+}
+
+// Show vector
+template <typename T>
+ostream & operator <<(ostream &os, const vector<T> &v) {
+    os << "[";
+    forn(i, v.size()) {
+        if (i > 0) os << ",";
+        os << v[i];
+    }
+    return os << "]";
+}
+
+// Show set
+template <typename T>
+ostream & operator <<(ostream &os, const set<T> &s) {
+    os << "{";
+    for(auto it = s.begin(); it != s.end(); it++){
+        if(it != s.begin()) os << ",";
+        os << *it;
+    }
+    return os << "}";
+}
+
+// ############################################################### //
+
+int mod(int a, int m = MOD){
+	int res = a % m;
+	if (res < 0) res += m;
+	return res;
+}
+
+int addMod(int a, int b, int m = MOD){
+	int res = mod(a, m) + mod(b, m);
+	return mod(res, m);
+}
+
+int n, c; 
+
+void solve(){
+	vi neighbor(n+1); forsn(i, 1, n+1) cin >> neighbor[i];
+	
+	MOD = c;
+	vi prefixSum(SIZE(neighbor));
+	map<int, vi> prefixWithValue;
+	prefixWithValue[0] = {0};
+	forsn(i, 1, n+1) {
+		prefixSum[i] = addMod(prefixSum[i-1], neighbor[i]);
+		prefixWithValue[prefixSum[i]].pb(i);
+	}
+	
+	for (auto [_, vec] : prefixWithValue){
+		if (SIZE(vec) == 1) continue;
+		forsn(i, vec[0]+1, vec[1]+1) cout << i << " ";
+		cout << "\n";
+		return ;
+	}
+}
+
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+		
+	int t = 1; 
+	//~ cin >> t;
+	//~ forn(_, t) solve();
+	
+	while (cin >> c >> n){
+		if (c == 0) break;
+		solve();
+	}
+	
+	return 0;
+}
