a56afcc307759ce4d76325bced2f96d4b5bd9e4a
FacundoCG <coronelfacundo30@gmail.com>
2026-06-24 15:29:34 -0300

    Luchadores japoneses

---
+++ Homework/LuchadoresJaponeses.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using ii = pair<ll,ll>;
+using vii = vector<ii>;
+using vvii = vector<vii>;
+
+template <typename T>
+using vv = vector<vector<T>>;
+
+const ll UNDEFINED = -1;
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const ll zero = 0;
+const ld EPSILON = 1e-10;
+const double PI = acos(-1.0);
+
+#define pb push_back
+#define fst first
+#define snd second
+#define esta(x,c) ((c).find(x) != (c).end())  // Devuelve true si x es un elemento de c.
+#define all(c) (c).begin(),(c).end()
+#define SIZE(c) int((c).size())
+
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define RAYA cerr << "----------" << endl
+
+#define forn(i,n) for (int i=0;i<(int)(n);i++)
+#define forsn(i,s,n) for (int i=(s);i<(int)(n);i++)
+#define dforn(i,n) for(int i=(int)((n)-1);i>=0;i--)
+#define dforsn(i,s,n) for(int i=(int)((n)-1);i>=(int)(s);i--)
+
+// Show pair
+template <typename T1, typename T2>
+ostream & operator <<(ostream &os, const pair<T1, T2> &p) {
+    os << "{" << p.first << "," << p.second << "}";
+    return os;
+}
+
+// Show vector
+template <typename T>
+ostream & operator <<(ostream &os, const vector<T> &v) {
+    os << "[";
+    forn(i, v.size()) {
+        if (i > 0) os << ",";
+        os << v[i];
+    }
+    return os << "]";
+}
+
+// Show set
+template <typename T>
+ostream & operator <<(ostream &os, const set<T> &s) {
+    os << "{";
+    for(auto it = s.begin(); it != s.end(); it++){
+        if(it != s.begin()) os << ",";
+        os << *it;
+    }
+    return os << "}";
+}
+
+// ############################################################### //
+
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+
+template <typename T>
+using ordered_multiset = tree<
+    pair<T, int>,
+    null_type,
+    less<pair<T, int>>, 
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
+
+void solve(){
+	int n; cin >> n;
+	
+	vector<pair<ii, int>> fighter(n);
+	
+	forn(i, n) {
+		cin >> fighter[i].fst.fst >> fighter[i].fst.snd;
+		fighter[i].snd = i;
+	}
+	
+	sort(all(fighter)); // Los ordeno por peso
+
+	vl height(n); forn(i, n) height[i] = fighter[i].fst.snd;
+	vl res(n);
+
+	ordered_multiset<ll> heightsOfSmallerOrEqualWeight;
+	heightsOfSmallerOrEqualWeight.insert(make_pair(height[0], 0));
+		
+	forsn(i, 1, n){
+		ll index = fighter[i].snd;		
+		ll smallerHeights = heightsOfSmallerOrEqualWeight.order_of_key(make_pair(height[i]+1, -1)); // # elementos < currentHeight
+		res[index] += smallerHeights;
+		heightsOfSmallerOrEqualWeight.insert(make_pair(height[i], i));
+	}
+		
+	forn(i, n) cout << res[i] << "\n";
+}
+
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+	
+	int t = 1; 
+	//~ cin >> t;
+	forn(_, t) solve();
+	return 0;
+}
